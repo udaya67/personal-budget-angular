82ec7c96d19f9661e7b462dbef07e7861ad9a2c7
udaya <udayach67@gmail.com>
2025-02-16 17:51:07 -0500

    Completed 1 and 2 in instructions

--- personal-budget/src/app/hero/hero.component.ts
+++ personal-budget/src/app/hero/hero.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'pb-hero',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss'
 })
